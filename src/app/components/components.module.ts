6e2caab1daf0898e2fbc67ce866ebc54f7aa491c
Roderick777 <38777569+Roderick777@users.noreply.github.com>
2020-05-25 23:13:40 -0400

    limpieza

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CrearComponent } from './crear/crear.component';
-import { ListadoComponent } from './listado/listado.component';
 import { FormsModule } from '@angular/forms'
 
 
 
 @NgModule({
-  declarations: [CrearComponent, ListadoComponent],
+  declarations: [CrearComponent],
   imports: [
     CommonModule,
     FormsModule
